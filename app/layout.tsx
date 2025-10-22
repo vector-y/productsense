0aa697be45f120f600e81634328d29d838c92cd8
vector-y <vt.victortran@gmail.com>
2025-10-22 14:46:23 -0700

    Fonts implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,43 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+// Primary font: Kyoto
+const kyoto = localFont({
+	src: [
+		{
+			path: "../public/fonts/PPKyoto-Regular.otf",
+			weight: "400",
+			style: "normal",
+		},
+		{
+			path: "../public/fonts/PPKyoto-RegularItalic.otf",
+			weight: "400",
+			style: "italic",
+		},
+		{
+			path: "../public/fonts/PPKyoto-Medium.otf",
+			weight: "500",
+			style: "normal",
+		},
+		{
+			path: "../public/fonts/PPKyoto-Bold.otf",
+			weight: "700",
+			style: "normal",
+		},
+		{
+			path: "../public/fonts/PPKyoto-BoldItalic.otf",
+			weight: "700",
+			style: "italic",
+		},
+	],
+	variable: "--font-kyoto",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+// Secondary font: Inter
+const inter = localFont({
+	src: "../public/fonts/Inter-VariableFont_opsz,wght.ttf",
+	variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +53,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${kyoto.variable} ${inter.variable} antialiased`}
       >
         {children}
       </body>
